5db454161ecc28ad054358769b46c23582777bd4
kwank6704 <nattarat.kwan@gmail.com>
2026-07-29 23:09:22 +0700

    Remove duplicate import statement for PathVariable in ResearcherController

--- src/main/kotlin/com/cocoa/web/controller/ResearcherController.kt
+++ src/main/kotlin/com/cocoa/web/controller/ResearcherController.kt
@@ -18,11 +18,11 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.PathVariable
 import java.util.UUID
 
 @RestController
